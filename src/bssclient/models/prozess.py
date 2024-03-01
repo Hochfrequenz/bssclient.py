b7a01eb16ce1d84ef2fcffda0f44255a4e5589de
konstantin <konstantin.klein@hochfrequenz.de>
2024-03-01 09:45:36 +0100

    make even more fields nullable (#10)
    fix more types

--- src/bssclient/models/prozess.py
+++ src/bssclient/models/prozess.py
@@ -18,11 +18,11 @@ class Prozess(BaseModel):
     typ: str
     ausloeser: str
     externe_id: str = Field(alias="externeId")
-    marktlokation: str
-    ide_referenz: str = Field(alias="ideReferenz")
-    transaktionsgrund: str
+    marktlokation: str | None = None
+    ide_referenz: str | None = Field(alias="ideReferenz", default=None)
+    transaktionsgrund: str | None = None
     ausloeser_daten: str = Field(alias="ausloeserDaten")
-    antwort_status: str = Field(alias="antwortStatus")
+    antwort_status: str | None = Field(alias="antwortStatus", default=None)
     einheit: str | None = None
-    messlokation: str
-    zaehlernummer: str
+    messlokation: str | None = None
+    zaehlernummer: str | None = None

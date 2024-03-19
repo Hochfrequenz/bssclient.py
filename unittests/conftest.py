f5e2710f6689fabc761916ec89c8ca721c129e67
konstantin <konstantin.klein@hochfrequenz.de>
2024-03-19 17:33:52 +0100

    add oauth

--- unittests/conftest.py
+++ unittests/conftest.py
@@ -4,20 +4,40 @@ import pytest
 from yarl import URL
 
 from bssclient import BssClient, BssConfig
+from bssclient.client.bssclient import BasicAuthBssClient, OAuthBssClient
+from bssclient.client.config import BasicAuthBssConfig, OAuthBssConfig
 
 
 @pytest.fixture
-async def bss_client_with_default_auth() -> AsyncGenerator[tuple[BssClient, BssConfig], None]:
+async def bss_client_with_basic_auth() -> AsyncGenerator[tuple[BssClient, BssConfig], None]:
     """
     "mention" this fixture in the signature of your test to run the code up to yield before the respective test
     (and the code after yield the test execution)
     :return:
     """
-    bss_config = BssConfig(
+    bss_config = BasicAuthBssConfig(
         server_url=URL("https://bss.inv/"),
         usr="my-usr",
         pwd="my-pwd",
     )
-    client = BssClient(bss_config)
+    client = BasicAuthBssClient(bss_config)
+    yield client, bss_config
+    await client.close_session()
+
+
+@pytest.fixture
+async def bss_client_with_oauth() -> AsyncGenerator[tuple[BssClient, BssConfig], None]:
+    """
+    "mention" this fixture in the signature of your test to run the code up to yield before the respective test
+    (and the code after yield the test execution)
+    :return:
+    """
+    bss_config = OAuthBssConfig(
+        server_url=URL("https://basicsupply.invalid.de/"),
+        client_id="my-client-id",
+        client_secret="my-client-secret",
+        token_url="https://validate-my-token.inv",
+    )
+    client = OAuthBssClient(bss_config)
     yield client, bss_config
     await client.close_session()

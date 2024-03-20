fcc97d50725fe52f91b70c9b9646fb191b08feed
konstantin <konstantin.klein@hochfrequenz.de>
2024-03-20 07:32:08 +0100

    feat: add possibility to use an existing bearer token (w/o client+secret)

--- src/bssclient/client/config.py
+++ src/bssclient/client/config.py
@@ -2,7 +2,7 @@
 contains a class with which the BSS client is instantiated/configured
 """
 
-from pydantic import BaseModel, ConfigDict, HttpUrl, field_validator
+from pydantic import BaseModel, ConfigDict, HttpUrl, field_validator, model_validator
 from yarl import URL
 
 
@@ -78,13 +78,27 @@ class OAuthBssConfig(BssConfig):
     Url of the token endpoint; e.g. 'https://lynqtech-dev-auth-server.auth.eu-central-1.amazoncognito.com/oauth2/token'
     """
 
-    # pylint:disable=no-self-argument
-    @field_validator("client_id", "client_secret")
-    def validate_string_is_not_empty(cls, value):
+    bearer_token: str | None = None
+    """
+    You may optionally provide a 'hardcoded' bearer token here. As long as its valid, it's used for requests and the
+    client id and secret are not used.
+    This is useful when you have ways to get a token but not a client id and secret.
+    """
+
+    @model_validator(mode="after")
+    def check_secret_or_token_is_present(cls, values):  # pylint:disable=no-self-argument
         """
-        Check that no one tries to bypass validation with empty strings.
-        If we had wanted that you can omit values, we had used Optional[str] instead of str.
+        Ensures that either (id+secret) or a bare token are present
         """
-        if not value.strip():
-            raise ValueError("my_string cannot be empty")
-        return value
+        token_is_present = values.bearer_token is not None and values.bearer_token.strip()
+        client_id_and_secret_are_present = (
+            values.client_id is not None
+            and values.client_id.strip()
+            and values.client_secret is not None
+            and values.client_secret.strip()
+        )
+        if not token_is_present and not client_id_and_secret_are_present:
+            raise ValueError(
+                # pylint:disable=line-too-long
+                f"You must provide either cliend id and secret or a bearer token, but not None of both"
+            )

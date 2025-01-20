53da4ec83b0288af9bdd0eb3e0d6eb688230b1c9
Konstantin <konstantin.klein+github@hochfrequenz.de>
2025-01-20 11:39:10 +0100

    feat: add method to download events from Event Controller

--- unittests/test_bss_client.py
+++ unittests/test_bss_client.py
@@ -1,8 +1,14 @@
+import json
+import uuid
+from pathlib import Path
+
 import pytest
+from aioresponses import aioresponses
 from yarl import URL
 
 from bssclient.client.bssclient import BasicAuthBssClient
 from bssclient.client.config import BasicAuthBssConfig, OAuthBssConfig
+from bssclient.models.events import EventHeader
 
 
 @pytest.mark.parametrize(
@@ -27,3 +33,17 @@ def test_get_tld(actual_url: URL, expected_tld: URL):
 def test_oauth_config():
     with pytest.raises(ValueError):
         OAuthBssConfig(server_url=URL("https://bss.example.com"), bearer_token="something-which-is-definittly no token")
+
+
+async def test_get_events(bss_client_with_basic_auth, caplog) -> None:
+    client, bss_config = bss_client_with_basic_auth
+    random_guid = uuid.uuid4()
+    client.get_events("Prozess", random_guid)
+    stats_json_file = Path(__file__).parent / "example_data" / "prozess-events.json"
+    with open(stats_json_file, "r", encoding="utf-8") as infile:
+        response_body = json.load(infile)
+    with aioresponses() as mocked_bss:
+        mocked_get_url = f"{bss_config.server_url}api/Event/Prozess/{random_guid}"
+        mocked_bss.get(mocked_get_url, status=200, payload=response_body)
+        actual = await client.get_events("Prozess", random_guid)
+    assert all(isinstance(x, EventHeader) for x in actual)
